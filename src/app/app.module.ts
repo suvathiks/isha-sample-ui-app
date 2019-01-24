32d18e1675955891ffc971d16beb73d7cb919712
ragavyarasi <ragav.code@gmail.com>
2019-01-24 15:17:52 +0530

    Just added the SDK module and the api component as a component that is imported in this module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ApiComponent } from './SdkModule/api/api.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApiComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/sdk/sdk.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ApiComponent } from './features/api/api.component';
+
+@NgModule({
+  declarations: [ApiComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SdkModule { }
